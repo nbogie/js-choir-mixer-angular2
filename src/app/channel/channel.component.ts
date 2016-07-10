8a74fcf530c5a472ae10efe4d03445e2bbbe76fd
Neill Bogie <neillbogie@googlemail.com>
2016-07-10 02:32:00 +0100

    generate mixer and channel components

---
+++ src/app/channel/channel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-channel',
+  templateUrl: 'channel.component.html',
+  styleUrls: ['channel.component.css']
+})
+export class ChannelComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/mixer/mixer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-mixer',
+  templateUrl: 'mixer.component.html',
+  styleUrls: ['mixer.component.css']
+})
+export class MixerComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
