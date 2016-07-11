2ec8ceadf7655b9c05c2c71fe9886822d52783b3
Neill Bogie <neillbogie@googlemail.com>
2016-07-11 19:43:57 +0100

    channel has a src, gain, and analyser, connected.

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -10,17 +10,64 @@ export class ChannelComponent implements OnInit {
   
   @Input() channelInfo: ChannelInfo;
   @Input() context: AudioContext;
-  
+  isMuted: boolean = false;
+  srcNode: AudioNode;
+  gainNode: GainNode;
+  //fft stuff
+  analyser: any;
+  fftConfig:any;
+  dataArray: Uint8Array;
+
   constructor() {}
 
   ngOnInit() {
-  }
+
+    let fftConfigs = {
+      waveform: {
+          type: "waveform",
+          size: 1024
+      },
+      spectrum: {
+          type: "spectrum",
+          size: 128
+      }
+    };
+    this.fftConfig = fftConfigs.waveform;
+}
 
   play() {
       let src = this.context.createBufferSource();
-      src.playbackRate.value = 1;
       src.buffer = this.channelInfo.buffer;
-      src.connect(this.context.destination);
+      src.playbackRate.value = 1;
+
+      let gainNode = this.context.createGain();
+      gainNode.gain.value = 1;
+      
+      src.connect(gainNode);
+      gainNode.connect(this.context.destination);
+      
+      //TODO: only the src node need by recreated on each play.
+      //The rest just needs to be reconnected.
+      let analyser = this.context.createAnalyser();
+      analyser.fftSize = this.fftConfig.size;
+      let bufferLength = analyser.frequencyBinCount;
+      let dataArray = new Uint8Array(bufferLength);
+      gainNode.connect(analyser);
+      
       src.start();
+
+      //store those elements we'll need to reference
+      this.srcNode = src;
+      this.gainNode = gainNode;
+      this.analyser = analyser;
+      this.dataArray = dataArray;          
   }
+
+  setGain(v: number) {
+    this.gainNode.gain.cancelScheduledValues(this.context.currentTime);
+    this.gainNode.gain.value = v;
+    //TODO: update slider?
+  }
+
+
 }
